1aa04f6dc03b609922b4e3a71eb768d1ed5fc26b
prashant <pchaturvedi@g2.com>
2026-05-28 12:32:54 +0530

    Update portfolio details and change URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
-  site: "https://posi2.github.io",
+  site: "https://prashant.github.io",
   output: "static",
   compressHTML: true,
   build: {
